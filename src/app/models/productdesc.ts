2d2b692070b798ba95e1cd5ef238829ab61905e2
MWK <maxwa95@programmer.net>
2018-06-10 22:26:22 +0200

    fix conflict

--- src/app/models/productdesc.ts
+++ src/app/models/productdesc.ts
@@ -1,10 +1,6 @@
 import {product}from './product'
 import {Description}from './Description'
-<<<<<<< HEAD
-export class productdesc {
-=======
 export class productdesc{
->>>>>>> bba8c42bc21e5feadd5b4f74c26fa1176434eaef
 product:product =new product ()
 Description: Description= new Description() 
     
