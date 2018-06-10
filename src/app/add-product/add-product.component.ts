5ce95f2fce9f42b46fbf9a0b1f64090f7b9f9f2c
MWK <maxwa95@programmer.net>
2018-06-10 22:18:16 +0200

    fix

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { CookieService } from 'ngx-cookie-service';
-=======
 import {productdesc}from '../models/productdesc'
->>>>>>> bba8c42bc21e5feadd5b4f74c26fa1176434eaef
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent implements OnInit {
-<<<<<<< HEAD
-
+  
+  prod:productdesc=new productdesc()
   constructor(private cookieService : CookieService) { 
    if (this.cookieService.get("access_token") == null) {
     // force him to  go to login page here
@@ -19,10 +16,6 @@ export class AddProductComponent implements OnInit {
 
 
   }
-=======
-prod:productdesc=new productdesc()
-  constructor() { }
->>>>>>> bba8c42bc21e5feadd5b4f74c26fa1176434eaef
 
   ngOnInit() {
   }
