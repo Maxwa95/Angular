bba8c42bc21e5feadd5b4f74c26fa1176434eaef
eslamghoniem <eslamghoniem@users.noreply.github.com>
2018-06-10 22:14:53 +0200

    add product html
    add prod

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {productdesc}from '../models/productdesc'
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent implements OnInit {
-
+prod:productdesc=new productdesc()
   constructor() { }
 
   ngOnInit() {
