8201bacc37374ddd79bd9c7ff161a3899e7652f9
eslamghoniem <eslamghoniem@users.noreply.github.com>
2018-06-10 22:57:27 +0200

    htm
    html

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import {productdesc}from '../models/productdesc'
+import {productdesc}from '../models/productdesc';
 import {  DataserviceService} from "../dataservice.service";
+import {Category}from '../models/category';
+import {brand}from '../models/brands';
+import {model}from '../models/model';
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent implements OnInit {
+  cates:Category[]=[];
+  brands:brand[]=[];
+  models:model[]=[]
   
   prod:productdesc=new productdesc()
-  constructor(private cookieService : CookieService,private DataserviceService ) { 
+  constructor(private cookieService : CookieService,private Data : DataserviceService) { 
    if (this.cookieService.get("access_token") == null) {
     // force him to  go to login page here
    }
@@ -19,6 +25,16 @@ export class AddProductComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.Data.GetCategories().subscribe(
+      a=>this.cates=a.json(),e=>alert(e),null
+    )
+    this.Data.GetBrands().subscribe(
+      a=>this.brands=a.json(),e=>alert(e),null
+    )
+    this.Data.GetModels().subscribe(
+      a=>this.models=a.json(),e=>alert(e),null
+    )
+
   }
 
 }
