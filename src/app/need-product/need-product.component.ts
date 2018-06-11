9888b7ce98b27fa7dd7eb719917ad0af54237aa6
Aya Atiya <ayaatiya@Ayas-MacBook-Pro.local>
2018-06-11 12:56:16 +0200

    a

---
+++ src/app/need-product/need-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-need-product',
+  templateUrl: './need-product.component.html',
+  styleUrls: ['./need-product.component.scss']
+})
+export class NeedProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
