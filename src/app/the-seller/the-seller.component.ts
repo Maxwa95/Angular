08e01452be27e2eb3b5be227e14b576e5a3f336b
MWK <maxwa95@programmer.net>
2018-06-13 17:47:26 +0200

    crud

--- src/app/the-seller/the-seller.component.ts
+++ src/app/the-seller/the-seller.component.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import { DataserviceService } from '../dataservice.service';
-
+import { Router } from '@angular/router';
+import { DataserviceService } from "../dataservice.service";
+import { productsbycompany } from "../models/productsbycompany";
+import { CookieService } from 'ngx-cookie-service';
+import { product } from '../models/product';
+import { brand } from '../models/brands';
+import { model } from '../models/model';
 @Component({
   selector: 'app-the-seller',
   templateUrl: './the-seller.component.html',
   styleUrls: ['./the-seller.component.scss']
 })
 export class TheSellerComponent implements OnInit {
+ public products :Promise<product[]>;
+ public prods : product[] = []
+ public token : string = ""
 
-  constructor(data : DataserviceService) {
+  constructor(public router : Router,public data:DataserviceService,public cookie : CookieService) {
+    this.token = this.cookie.get("access_token")
+this.products = this.data.Getproducts(this.cookie.get("access_token")).then(
+a=>{
+  console.log(a.json())
+  return a.json()
+}
+)
 
-    
-   }
+this.products.then(
+a=>{
+  this.prods = a } 
+).catch(e=>{alert(e)})
+   
+}
 
   ngOnInit() {
 
   }
 
+  edit(prodid : number){
+    alert(prodid)
+  this.router.navigate([`/editProduct/${prodid}`]);
+  }
+  delete(prodid : number){
+   
+ if  (confirm("Are you sure to delete this product ?"))
+ {
+  this.data.DeleteProduct(prodid,this.token).subscribe(
+    a=>alert(a),
+    e=>alert(e)
+  )
+
+ }else
+ return false;
+
+  }
 }
